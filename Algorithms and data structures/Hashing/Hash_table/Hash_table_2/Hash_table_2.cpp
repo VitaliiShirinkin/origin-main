9108f858611bd0f50c3692f4dc37c06aeb4e02ff
Vitalii Shirinkin <artoiv2@mail.ru>
2023-07-26 12:34:07 +0300

    Добавил хеш-таблицу и красно-черное дерево

---
+++ Algorithms and data structures/Hashing/Hash_table/Hash_table_2/Hash_table_2.cpp
@@ -0,0 +1,43 @@
+﻿//Хеш-таблица(перемешивание)
+//Ввод перечня слов, создание хеш-таблицы и вывод
+
+#include <iostream>
+#include <unordered_set>
+#include <string>
+#include <Windows.h>
+
+void set_example()
+{
+    std::unordered_set<std::string> words;//контейнер для хеш-таблицы
+    std::string word;
+
+    getline(std::cin, word);
+    while (word != "") {
+        words.insert(word);//вставка слов в виде перечня через enter
+        getline(std::cin, word);
+    }
+
+    for (auto& word : words) {
+        std::cout << word << '\t';
+    }
+    std::cout << std::endl;
+
+    std::cout << "Введите ключ для поиска: ";
+    std::cin >> word;
+
+    if (words.find(word) != words.end()) {//поиск искомого слова по ключу
+        std::cout << "Найдено!\n";
+    }
+    else {
+        std::cout << "Не найдено...\n";
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    set_example();
+    return 0;
+}
---
+++ Algorithms and data structures/Pyramids_and_search_trees/Red_black_trees/Set/Set.cpp
@@ -0,0 +1,43 @@
+﻿// Set.cpp : Красно-черные деревья
+//В консоли вводятся слова, затем ищется из перечня слово
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <Windows.h>
+
+void set_example()
+{
+    std::set<std::string> words;//множество слов
+    std::string word;
+
+    getline(std::cin, word);
+    while (word != "") {
+        words.insert(word);//вставка слов в перечень
+        getline(std::cin, word);
+    }
+
+    for (auto& word : words) {//распечатка перечня слов с помощью цикла по алфавиту
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Введите ключ для поиска в наборе: ";
+    std::cin >> word;
+
+    if (words.find(word) != words.end()) {//поиск слов по ключу, пока слова не закончились
+        std::cout << "Найдено!\n";
+    }
+    else {
+        std::cout << "Не найдено...\n";
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    set_example();
+    return 0;
+}
