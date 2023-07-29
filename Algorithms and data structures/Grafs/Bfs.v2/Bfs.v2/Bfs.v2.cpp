fdaec3ec73adcc1ef8f3aabcda182c980542da81
Vitalii Shirinkin <artoiv2@mail.ru>
2023-07-29 19:19:57 +0300

    Добавил файл Dfs.v2 обход в глубину на основе класса

---
+++ Algorithms and data structures/Grafs/Bfs.v2/Bfs.v2/Bfs.v2.cpp
@@ -0,0 +1,95 @@
+﻿// Код C++ для печати обхода BFS из заданной исходной вершины 
+
+#include <vector>
+#include <list>
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+// Этот класс представляет ориентированный граф, используя 
+// представление списка смежности
+class Graph {
+
+	// Количество вершин 
+	int V;
+
+	// Указатель на массив, содержащий списки смежности 
+	vector<list<int> > adj;
+
+public:
+	// Constructor
+	Graph(int V);
+
+	// Функция добавления ребра к графу 
+	void addEdge(int v, int w);
+
+	// Выводит обход BFS из заданного источника s 
+	void BFS(int s);
+};
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj.resize(V);
+}
+
+void Graph::addEdge(int v, int w)
+{
+	// Добавим w в список v. 
+	adj[v].push_back(w);
+}
+
+void Graph::BFS(int s)
+{
+	// Отметим все вершины, как не посещенные 
+	vector<bool> visited;
+	visited.resize(V, false);
+
+	// Создим очередь для BFS 
+	list<int> queue;
+
+	// Пометим текущий узел как посещенный и поставим его в очередь 
+	visited[s] = true;
+	queue.push_back(s);
+
+	while (!queue.empty()) {
+
+		// Удалим вершину из очереди и распечатаем ее 
+		s = queue.front();
+		cout << s << " ";
+		queue.pop_front();
+
+		/*Получим все соседние вершины, удаленной из очереди, вершины s.
+			Если соседний узел не был посещен,
+			то отметьм его посещенным и поставим в очередь*/
+		for (auto adjacent : adj[s]) {
+			if (!visited[adjacent]) {
+				visited[adjacent] = true;
+				queue.push_back(adjacent);
+			}
+		}
+	}
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	// Создим график, указанный на приведенной выше диаграмме.
+	Graph g(4);
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 0);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
+
+
+
+	cout << "Ниже приведен первый обход в ширину "
+		<< "(начиная с вершины 2) \n";
+	g.BFS(2);
+
+	return 0;
+}
---
+++ Algorithms and data structures/Grafs/Dfs.v2/Dfs.v2/Dfs.v2.cpp
@@ -0,0 +1,125 @@
+﻿// Dfs.v2.cpp : Проход графа в глубину на основе класса Graph
+//
+
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+class Graph
+{
+private:
+	int verteсes;
+	int** adjacency_matrix;
+	bool* used;
+
+private:
+	void matrix_filling(std::ifstream& in, int** adjacency_matrix)//заполнение матрицы смежности из файла In
+
+	{
+		while (!in.eof())
+		{
+			for (int i = 0; i < verteсes; i++)
+			{
+				for (int j = 0; j < verteсes; j++)
+				{
+					int value;
+					in >> value;
+					adjacency_matrix[i][j] = value;//и запись этих данных в массив
+				}
+			}
+			in.close();//закрытие файла
+		}
+	}
+
+public:
+	Graph()
+	{
+		std::ifstream in("in.txt");
+		in >> verteсes;
+
+		used = new bool[verteсes]();//массив для пройденных вершин
+
+		adjacency_matrix = new int* [verteсes];//для матрицы смежности
+		for (int i = 0; i < verteсes; i++)
+		{
+			adjacency_matrix[i] = new int[verteсes];
+		}
+
+		matrix_filling(in, adjacency_matrix);
+
+		
+	}
+	//Освобождение занятой памяти
+	template <typename T>
+	void deleter(T array) {
+		delete[] array;
+		array = nullptr;
+	}
+
+	~Graph() {
+		for (int i = 0; i < verteсes; ++i) {
+			delete[] adjacency_matrix[i];
+		}
+
+		deleter(adjacency_matrix);
+		deleter(used);
+	}
+
+	void Print()//вывод данных из массива матрицы смежности в консоль
+	{
+		std::cout << "Матрица смежности: " << '\n' << '\n';
+
+		for (int i = 0; i < verteсes; i++)
+		{
+			for (int j = 0; j < verteсes; j++)
+			
+				std::cout << adjacency_matrix[i][j] << " ";
+				std::cout << '\n';
+			
+		}
+		std::cout << '\n';
+	}
+
+	void dfs(int vertex)//обход графа в глубину
+	{
+		used[vertex] = true;
+		std::cout << vertex + 1 << " ";
+		for (int i = vertex; i < verteсes; i++)
+		{
+			for (int j = 0; j < verteсes; j++)
+			{
+				if (adjacency_matrix[i][j] == 1)
+				{
+					if (!used[j])
+					{
+						dfs(j);
+					}
+				}
+			}
+		}
+	}
+
+
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::ifstream fin("in.txt");
+
+	if (!fin.is_open()) {
+		std::cout << "Файл in поврежден!" << std::endl;
+		return 1;
+	}
+
+	int verteсes;
+	Graph* graph = new Graph();
+	graph->Print();
+
+	std::cout << "Порядок обхода вершин: ";
+	graph->dfs(3);
+	std::cout << '\n';
+	delete graph;
+}
