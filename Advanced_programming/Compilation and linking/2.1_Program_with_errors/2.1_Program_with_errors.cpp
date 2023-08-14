af4523c59cad9d3e2ed6c86cfc39287acf960666
Vitalii Shirinkin <artoiv2@mail.ru>
2023-08-14 19:18:41 +0300

    Ошибки в программном коде

---
+++ Advanced_programming/Compilation and linking/2.1_Program_with_errors/2.1_Program_with_errors.cpp
@@ -0,0 +1,30 @@
+﻿// 2.1_Program_with_errors.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+struct Point {
+private:
+	double m_x;
+	double m_y;
+public:
+	Point(double x, double y) {
+		m_x = x;
+		m_y = y;
+	}
+
+	void print_point(const Point& point_object) {
+		std::cout << "x:" << point_object.m_x << ", y: "
+			<< point_object.m_y << std::endl;
+	}
+};
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		Point my_point(i, 2 * i);
+		my_point.print_point(my_point);
+	}
+	return 0;
+};
---
+++ Advanced_programming/Compilation and linking/2.2_Solid_errors/2.2_Solid_errors.cpp
@@ -0,0 +1,56 @@
+﻿// 2.2_Solid_errors.cpp : Задача 2*. Сплошные ошибки
+//
+
+﻿//Cинтаксические ошибки:
+/*
+#include <iostream>
+
+int main()
+{
+	for (int i = 0; i < 10, ++i)								i<10;	вместо ; поставлена ,
+	{
+		if (i % 2) std::cout << i								i;		нет ;
+		if (i >= 5) std::cout << i " больше 5" << std::endl;	i << "	нет <<
+}																}		нет закрывающей фигурной скобки
+*/
+
+// Семантические ошибки:
+/*
+#include <iostream>
+
+int main()
+{
+	for (int i = 0; i < 10; +i)									++i		неправильная запись префиксного инкремента
+	{
+		if (i % 2) std::cout << i << std::endl;
+		if (i >= 5) std::cout << i << " больше 5" << std::endl;	больше 5	больше или равно
+	}
+
+}
+*/
+
+// Ошибка линковки:
+/*
+test.h
+int f(int n);
+
+
+test.cpp
+
+#include <iostream>
+#include "test.h"
+
+int f(bool n)														int f(int n)	объявленная фунция в заголовочном файле имеет другой тип параметров
+{}
+
+int Main()															int main()		строго написание main() маленькими строчными буквами
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		if (i % 2) std::cout << i << std::endl;
+		if (i >= 5) std::cout << i << " больше 5" << std::endl;
+		int m = f(i);
+	}
+	return 0;
+};
+*/
