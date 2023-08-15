508d401369d07e815951783a26716c9299c2fea4
Vitalii Shirinkin <artoiv2@mail.ru>
2023-08-15 20:38:47 +0300

    Исправление задачи 1.1 Лямбда-функция

--- Advanced_programming/1_1_Lambda_function/1_1_Lambda_function.cpp
+++ Advanced_programming/1_1_Lambda_function/1_1_Lambda_function.cpp
@@ -1,5 +1,7 @@
 ﻿// 1_1_Lambda_function.cpp : программа, которая все нечётные элементы массива (std::vector) умножает на 3 с помощью лямбда-функции. 
 //Циклы использовать не нужно.
+    //for_each Применяет заданный объект функции к каждому элементу
+    //в прямом порядке в пределах диапазона и возвращает объект функции.
 
 #include <iostream>
 #include <Windows.h>
@@ -11,32 +13,20 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    std::vector<int> ivect {4, 7, 9, 14, 12};
+    std::vector<int> ivect {4, 7, 9, 14, 12, 2, 21, 14};
+
 
     std::cout << "Входные данные: ";
-
-    //for_each Применяет заданный объект функции к каждому элементу
-    //в прямом порядке в пределах диапазона и возвращает объект функции.
-
     std::for_each
-    (ivect.begin(), ivect.end(), [](const int& ivect)
-        {
-            std::cout << '\t' << ivect;
-        }
-    );
+    (ivect.begin(), ivect.end(), [](const int& n) {std::cout << '\t' << n;});
     std::cout << std::endl;
 
-    std::cout << "Выходные данные: ";
 
-    std::for_each
-    (ivect.begin(), ivect.end(), [&ivect](const int& ivect)
-        {
-            if(ivect %2 !=0)
-            std::cout << '\t' << ivect * 3;
-            else
-            std::cout << '\t' << ivect;
-        }
-    );
+    std::cout << "Выходные данные: ";
+    auto itertns = ivect.begin();
+
+    std::for_each(ivect.begin(), ivect.end(), [&itertns](const int& n) {if(1 == n %2) {*itertns = n * 3;} ++itertns;});
+    std::for_each(ivect.begin(), ivect.end(), [](const int& n) {std::cout << '\t' << n;});
 
     return 0;
 }
